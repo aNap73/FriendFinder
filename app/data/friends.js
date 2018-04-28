df9a9aea93b5b8e75517c462236b8b55035c31e2
aNap73 <napolitano.anthony@yahoo.com>
2018-04-28 03:29:50 -0400

    worked on data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,107 @@
 module.exports = function() {
   // Requests
   let friends = [
-                   {Name:'Mama Mia',
-                   Quest: [1,2,3,4,5,1,2,3,4,1],
-                   Link: 'https://media2.giphy.com/media/3iiwqPF9noqdy/giphy.gif'},
-                   {Name:'Love Man',
-                   Quest: [1,2,3,4,5,1,2,3,4,2],
-                   Link: 'https://i.ytimg.com/vi/OVpTmelPqRI/hqdefault.jpg'},
-                   {Name:'Surfdog',
-                   Quest: [1,2,3,4,5,1,2,3,4,3],
-                   Link: 'http://www.likable.info/img/d/o/dog-surfing-gif-animation.png'},
-                  {Name:'Anthony',
-                  Quest: [2,2,4,5,4,5,5,3,5,3],
-                  //Quest: [1,2,3,4,5,1,2,3,4,1],
-                  Link: 'https://media.licdn.com/dms/image/C4D03AQEeC3DS_6RWgg/profile-displayphoto-shrink_200_200/0?e=1530039600&v=beta&t=jtEThWk93undUZrXKrQ-D6J8fI63wV47WOPxO8Pj3yA'}
+ 
+    {
+      "Name": "Mama Mia",
+      "Quest": [
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+      ],
+      "Link": "https://media2.giphy.com/media/3iiwqPF9noqdy/giphy.gif"
+      },
+      {
+      "Name": "Love Man",
+      "Quest": [
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2
+      ],
+      "Link": "https://i.ytimg.com/vi/OVpTmelPqRI/hqdefault.jpg"
+      },
+      {
+      "Name": "Surfdog",
+      "Quest": [
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4
+      ],
+      "Link": "http://www.likable.info/img/d/o/dog-surfing-gif-animation.png"
+      },  
+      {
+      "Name": "Errma Gerd okMyGerd",
+      "Quest": [
+      "5",
+      "1",
+      "1",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "3"
+      ],
+      "Link": "https://media.giphy.com/media/l2QZPcdfgkSiB7TMI/giphy.gif"
+      },
+      {
+      "Name": "Rock McHardson",
+      "Quest": [
+      "3",
+      "5",
+      "2",
+      "3",
+      "2",
+      "4",
+      "1",
+      "2",
+      "5",
+      "5"
+      ],
+      "Link": "https://i.warosu.org/data/tg/img/0399/31/1431565742729.gif"
+      },
+      {
+        "Name": "Anthony",
+        "Quest": [
+        2,
+        2,
+        4,
+        5,
+        4,
+        5,
+        5,
+        3,
+        5,
+        3
+        ],
+        "Link": "https://media.licdn.com/dms/image/C4D03AQEeC3DS_6RWgg/profile-displayphoto-shrink_200_200/0?e=1530039600&v=beta&t=jtEThWk93undUZrXKrQ-D6J8fI63wV47WOPxO8Pj3yA"
+      }  
                 ];  
   return friends;
   
 }
+
+
+
