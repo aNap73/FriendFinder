2e27fb62aab274bfbfd23e8faca415c5e5877abf
aNap73 <napolitano.anthony@yahoo.com>
2018-04-27 21:32:28 -0400

    preheroku2

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,7 @@
 
 module.exports = function(app) {
   // Requests 
-  console.log('inhtmlRoutes');
+  
   var path = require("path");
   app.get("/survey",function(req,res){  
     res.sendFile(path.join(__dirname, "../public/survey.html"));
